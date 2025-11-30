f56804372a32cab1b564f14916f0ab8a2e9e727a
shabesa <k.a.shabesa@gmail.com>
2025-12-01 04:24:22 +0530

    fixed file names

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ else
 	rm bin/*
 fi
 
-g++ -g -O0 -I . -o bin/interrupts_EP interrupts_student1_student2_EP.cpp
-g++ -g -O0 -I . -o bin/interrupts_RR interrupts_student1_student2_RR.cpp
-g++ -g -O0 -I . -o bin/interrupts_EP_RR interrupts_student1_student2_EP_RR.cpp
+g++ -g -O0 -I . -o bin/interrupts_EP interrupts_101258619_101166589_EP.cpp
+g++ -g -O0 -I . -o bin/interrupts_RR interrupts_101258619_101166589_RR.cpp
+g++ -g -O0 -I . -o bin/interrupts_EP_RR interrupts_101258619_101166589_EP_RR.cpp
